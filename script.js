b47f253741f29ed22d2068d2936f28af8d991941
unknown <nesad.omeragich27@gmail.com>
2021-03-16 00:07:26 +0100

    Finished CA Team Stats exercise

--- script.js
+++ script.js
@@ -24,13 +24,45 @@ const team = {
     },
     {
       opponent: 'Las Vegas Raiders',
-      teamPoints: 42,
-      opponentPoints: 27,
+      teamPoints: 35,
+      opponentPoints: 47,
     },
     {
       opponent: 'Green Bay Packers',
-      teamPoints: 42,
-      opponentPoints: 27,
+      teamPoints: 52,
+      opponentPoints: 50,
     },
   ],
+  get players() {
+    return this._players;
+  },
+  get games() {
+    return this._games;
+  },
+  addPlayer: function (firstName, lastName, age) {
+    const player = {
+      firstName: firstName,
+      lastName: lastName,
+      age: age,
+    };
+    this._players.push(player);
+  },
+  addGame: function (opponentName, teamPoints, opponentPoints) {
+    const game = {
+      opponent: opponentName,
+      teamPoints: teamPoints,
+      opponentPoints: opponentPoints,
+    };
+    this._games.push(game);
+  },
 };
+
+team.addPlayer('Steph', 'Curry', 28);
+team.addPlayer('Lisa', 'Leslie', 44);
+team.addPlayer('Bugs', 'Bunny', 76);
+
+team.addGame('Titans', 100, 98);
+team.addGame('Pittsburg Steelers', 77, 85);
+team.addGame('Kansas City Chiefs', 95, 90);
+
+console.log(team.games);
