60508c2e4c478452e286e2dcd652aec812661f73
unknown <nesad.omeragich27@gmail.com>
2021-03-27 21:07:03 +0100

    Completed another Promise exercise

--- script.js
+++ script.js
@@ -1,107 +1,34 @@
-const store = {
-  sunglasses: {
-    inventory: 817,
-    cost: 9.99,
-  },
-  pants: {
-    inventory: 236,
-    cost: 7.99,
-  },
-  bags: {
-    inventory: 17,
-    cost: 12.99,
-  },
-};
+// make a function that takes in a single parameter
+// and returns a new promise. using setTimeout,
+// after 500 milliseconds, the promise will either
+// resolove or reject. if the input is a string,
+// the promise resolves with that same string
+// uppercased. if the input is anything but a string
+// it rejects with that same input
+//
+// call the function delayedUpperCase
 
-const checkInventory = (order) => {
+// code goes here
+const delayedUpperCase = (input) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const itemsArr = order.items;
-      let inStock = itemsArr.every(
-        (item) => store[item[0]].inventory >= item[1]
-      );
-
-      if (inStock) {
-        let total = 0;
-        itemsArr.forEach((item) => {
-          total += item[1] * store[item[0]].cost;
-        });
-        console.log(
-          `All of the items are in stock. The total cost of the order is ${total}.`
-        );
-        resolve([order, total]);
+      if (typeof input === 'string') {
+        resolve(input.toUpperCase());
       } else {
-        reject(
-          `The order could not be completed because some items are sold out.`
-        );
+        reject(input);
       }
-    }, generateRandomDelay());
+    }, 500);
   });
 };
 
-const processPayment = (responseArray) => {
-  const order = responseArray[0];
-  const total = responseArray[1];
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let hasEnoughMoney = order.giftcardBalance >= total;
-      // For simplicity we've omited a lot of functionality
-      // If we were making more realistic code, we would want to update the giftcardBalance and the inventory
-      if (hasEnoughMoney) {
-        const balance = order.giftcardBalance - total;
-        console.log(
-          `Payment processed with giftcard. Generating shipping label.\n
-          Balance: ${balance}`
-        );
-        let trackingNum = generateTrackingNumber();
-        resolve([order, trackingNum]);
-      } else {
-        reject(`Cannot process order: giftcard balance was insufficient.`);
-      }
-    }, generateRandomDelay());
-  });
-};
-
-const shipOrder = (responseArray) => {
-  const order = responseArray[0];
-  const trackingNum = responseArray[1];
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(
-        `The order has been shipped. The tracking number is: ${trackingNum}.`
-      );
-    }, generateRandomDelay());
-  });
-};
-
-// This function generates a random number to serve as a "tracking number" on the shipping label. In real life this wouldn't be a random number
-function generateTrackingNumber() {
-  return Math.floor(Math.random() * 1000000);
-}
-
-// This function generates a random number to serve as delay in a setTimeout() since real asynchrnous operations take variable amounts of time
-function generateRandomDelay() {
-  return Math.floor(Math.random() * 2000);
-}
-
-const order = {
-  items: [
-    ['sunglasses', 1],
-    ['bags', 2],
-  ],
-  giftcardBalance: 79.82,
-};
-
-// Refactor the code below:
-
-checkInventory(order)
-  .then((resolvedValueArray) => {
-    return processPayment(resolvedValueArray);
-  })
-  .then((resolvedValueArray) => {
-    return shipOrder(resolvedValueArray);
-  })
-  .then((successMessage) => {
-    console.log(successMessage);
-  })
-  .catch((failureMessage) => console.log(failureMessage));
+delayedUpperCase('123')
+  .then(
+    (handleSuccess = (resolvedValue) => {
+      console.log('Success: ', resolvedValue);
+    })
+  )
+  .catch(
+    (handleFailure = (resolvedValue) => {
+      console.log('Failure: ', resolvedValue);
+    })
+  );
