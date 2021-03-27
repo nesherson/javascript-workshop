a3ae3b365e2245cd5638f38ab199ad99af915ff1
unknown <nesad.omeragich27@gmail.com>
2021-03-27 23:54:18 +0100

    Completed second set of exercises

--- 02-consuming-promises/answers.js
+++ 02-consuming-promises/answers.js
@@ -1,24 +1,38 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
+ *
  * @param {Promise} promise
  * @param {thunk} action
- * 
+ *
  */
-function waitForPromise(promise, action){
+function waitForPromise(promise, action) {
   /* IMPLEMENT ME */
+  promise.then(
+    (handleSuccess = (resolvedValue) => {
+      action(resolvedValue);
+    })
+  );
 }
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} promise 
- * @param {consumer} consumer 
- * @param {handler} handler 
+ *
+ * @param {Promise} promise
+ * @param {consumer} consumer
+ * @param {handler} handler
  */
-function consumePromise(promise, consumer, handler){
+function consumePromise(promise, consumer, handler) {
   /* IMPLEMENT ME! */
+  promise
+    .then(
+      (handleSuccess = (resolvedValue) => {
+        consumer(resolvedValue);
+      })
+    )
+    .catch((resolvedValue) => {
+      handler(resolvedValue);
+    });
 }
 
 /**
