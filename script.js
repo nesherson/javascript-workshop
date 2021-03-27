475b5bbcb10db2fe36ed7dacd61e58f5724eecc5
unknown <nesad.omeragich27@gmail.com>
2021-03-27 20:52:10 +0100

    CodeAcamdemy Promises exercise

--- script.js
+++ script.js
@@ -1,68 +1,107 @@
-const team = {
-  _players: [
-    {
-      firstName: 'Pablo',
-      lastName: 'Sanchez',
-      age: 11,
-    },
-    {
-      firstName: 'Adam',
-      lastName: 'Ericson',
-      age: 13,
-    },
-    {
-      firstName: 'David',
-      lastName: 'Abramov',
-      age: 12,
-    },
-  ],
-  _games: [
-    {
-      opponent: 'Broncos',
-      teamPoints: 42,
-      opponentPoints: 27,
-    },
-    {
-      opponent: 'Las Vegas Raiders',
-      teamPoints: 35,
-      opponentPoints: 47,
-    },
-    {
-      opponent: 'Green Bay Packers',
-      teamPoints: 52,
-      opponentPoints: 50,
-    },
-  ],
-  get players() {
-    return this._players;
+const store = {
+  sunglasses: {
+    inventory: 817,
+    cost: 9.99,
   },
-  get games() {
-    return this._games;
+  pants: {
+    inventory: 236,
+    cost: 7.99,
   },
-  addPlayer: function (firstName, lastName, age) {
-    const player = {
-      firstName: firstName,
-      lastName: lastName,
-      age: age,
-    };
-    this._players.push(player);
-  },
-  addGame: function (opponentName, teamPoints, opponentPoints) {
-    const game = {
-      opponent: opponentName,
-      teamPoints: teamPoints,
-      opponentPoints: opponentPoints,
-    };
-    this._games.push(game);
+  bags: {
+    inventory: 17,
+    cost: 12.99,
   },
 };
 
-team.addPlayer('Steph', 'Curry', 28);
-team.addPlayer('Lisa', 'Leslie', 44);
-team.addPlayer('Bugs', 'Bunny', 76);
+const checkInventory = (order) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const itemsArr = order.items;
+      let inStock = itemsArr.every(
+        (item) => store[item[0]].inventory >= item[1]
+      );
 
-team.addGame('Titans', 100, 98);
-team.addGame('Pittsburg Steelers', 77, 85);
-team.addGame('Kansas City Chiefs', 95, 90);
+      if (inStock) {
+        let total = 0;
+        itemsArr.forEach((item) => {
+          total += item[1] * store[item[0]].cost;
+        });
+        console.log(
+          `All of the items are in stock. The total cost of the order is ${total}.`
+        );
+        resolve([order, total]);
+      } else {
+        reject(
+          `The order could not be completed because some items are sold out.`
+        );
+      }
+    }, generateRandomDelay());
+  });
+};
 
-console.log(team.games);
+const processPayment = (responseArray) => {
+  const order = responseArray[0];
+  const total = responseArray[1];
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let hasEnoughMoney = order.giftcardBalance >= total;
+      // For simplicity we've omited a lot of functionality
+      // If we were making more realistic code, we would want to update the giftcardBalance and the inventory
+      if (hasEnoughMoney) {
+        const balance = order.giftcardBalance - total;
+        console.log(
+          `Payment processed with giftcard. Generating shipping label.\n
+          Balance: ${balance}`
+        );
+        let trackingNum = generateTrackingNumber();
+        resolve([order, trackingNum]);
+      } else {
+        reject(`Cannot process order: giftcard balance was insufficient.`);
+      }
+    }, generateRandomDelay());
+  });
+};
+
+const shipOrder = (responseArray) => {
+  const order = responseArray[0];
+  const trackingNum = responseArray[1];
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(
+        `The order has been shipped. The tracking number is: ${trackingNum}.`
+      );
+    }, generateRandomDelay());
+  });
+};
+
+// This function generates a random number to serve as a "tracking number" on the shipping label. In real life this wouldn't be a random number
+function generateTrackingNumber() {
+  return Math.floor(Math.random() * 1000000);
+}
+
+// This function generates a random number to serve as delay in a setTimeout() since real asynchrnous operations take variable amounts of time
+function generateRandomDelay() {
+  return Math.floor(Math.random() * 2000);
+}
+
+const order = {
+  items: [
+    ['sunglasses', 1],
+    ['bags', 2],
+  ],
+  giftcardBalance: 79.82,
+};
+
+// Refactor the code below:
+
+checkInventory(order)
+  .then((resolvedValueArray) => {
+    return processPayment(resolvedValueArray);
+  })
+  .then((resolvedValueArray) => {
+    return shipOrder(resolvedValueArray);
+  })
+  .then((successMessage) => {
+    console.log(successMessage);
+  })
+  .catch((failureMessage) => console.log(failureMessage));
