32f6f0eb9cd05bacf249771b585e31626bff3246
unknown <nesad.omeragich27@gmail.com>
2021-03-31 04:24:44 +0200

    Finished exercism exercise

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+function iterate(obj, path) {
+  if (obj.hasOwnProperty(path)) {
+    return obj[path];
+  }
+  for (const property in obj) {
+    if (typeof obj[property] === 'object') {
+      const value = iterate(obj[property], path);
+      if (value) return value;
+    }
+  }
+  return null;
+}
+
 function extract(object, path) {
   if (path.includes('.')) {
-    const property = path.split('.');
-    console.log(property);
-    return `${object}`;
+    const properties = path.split('.');
+    const lastProp = properties[properties.length - 1];
+
+    return iterate(object, lastProp);
   } else if (!path.includes('.') && object.hasOwnProperty(path)) {
     return object[path];
   }
   return null;
 }
-
 const TEAM = {
   name: 'Buzzer Beaters',
   coach: {
@@ -24,4 +37,4 @@ const TEAM = {
   },
 };
 
-console.log(extract(TEAM, 'coach.achievements.titles'));
+console.log(extract(TEAM, 'location.town'));
