4cb1736d29bf65328d56843160732c11c5e49ff4
unknown <nesad.omeragich27@gmail.com>
2021-04-18 23:43:09 +0200

    Addded reducer function

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const reducer = (state = initialWagonState, action) => {
+  switch (action.payload) {
+    case 'gather':
+      return {
+        ...state,
+        supplies: state.supplies + 15,
+        days: state.days + 1,
+      };
+    case 'travel':
+      const supplyCost = action.payload * 20;
+      const distanceTravelled = action.payload * 10;
+
+      if (supplyCost > state.supplies) {
+        return;
+      }
+
+      return {
+        supplies: state.supplies - supplyCost,
+        distance: state.distance + distanceTravelled,
+        days: action.payload,
+      };
+    case 'tippedWagon':
+      return {
+        ...state,
+        supplies: state.supplies - 30,
+        days: state.days + 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
 const initialWagonState = {
   supplies: 100,
   distance: 0,
