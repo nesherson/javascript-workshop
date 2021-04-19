d3da4509cd7ffc6fa6a791e6be2e5ffe92237c90
unknown <nesad.omeragich27@gmail.com>
2021-04-19 23:11:51 +0200

    Created initial project structure

--- script.js
+++ script.js
@@ -1,59 +1,48 @@
-const reducer = (state = initialWagonState, action) => {
+/* Note to learners: 
+Normally, you would import redux like this:
+
+  import { createStore } from 'redux';
+
+Due to Codecademy's technical limitations 
+for testing this exercise, we are using 
+`require()`.
+*/
+const { createStore } = require('redux');
+
+// Action Creators
+function increment() {
+  return { type: 'increment' };
+}
+
+function decrement() {
+  return { type: 'decrement' };
+}
+
+// Reducer / Store
+const initialState = 0;
+const countReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'gather':
-      return {
-        ...state,
-        supplies: state.supplies + 15,
-        days: state.days + 1,
-      };
-    case 'travel':
-      const supplyCost = action.payload * 20;
-      const distanceTravelled = action.payload * 10;
-
-      if (supplyCost > state.supplies) {
-        return;
-      }
-
-      return {
-        supplies: state.supplies - supplyCost,
-        distance: state.distance + distanceTravelled,
-        days: action.payload,
-      };
-    case 'tippedWagon':
-      return {
-        ...state,
-        supplies: state.supplies - 30,
-        days: state.days + 1,
-      };
-
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
     default:
       return state;
   }
 };
+const store = createStore(countReducer);
 
-const initialWagonState = {
-  supplies: 100,
-  distance: 0,
-  days: 0,
-};
+// HTML Elements
+const counterElement = document.getElementById('counter');
+const incrementer = document.getElementById('incrementer');
+const decrementer = document.getElementById('decrementer');
 
-let wagon = reducer(undefined, {});
-console.log('Starting wagon: ', wagon);
+// Store State Change Listener
+const render = () => {};
 
-wagon = reducer(wagon, {
-  type: 'travel',
-  payload: 1,
-});
-console.log('Wagon after travelling: ', wagon);
+// DOM Event Handlers
+const incrementerClicked = () => {};
+incrementer.addEventListener('click', incrementerClicked);
 
-wagon = reducer(wagon, { type: 'gather' });
-console.log('Wagon after gathering: ', wagon);
-
-wagon = reducer(wagon, { type: 'tippedWagon' });
-console.log('Wagon after tipping: ', wagon);
-
-wagon = reducer(wagon, {
-  type: 'travel',
-  payload: 3,
-});
-console.log('Wagon after travelling for 3 days: ', wagon);
+const decrementerClicked = () => {};
+decrementer.addEventListener('click', decrementerClicked);
