78ad6256ea5121ef753cd0d70f5f8c5fef6a6757
unknown <nesad.omeragich27@gmail.com>
2021-05-27 02:36:09 +0200

    Express middleware exercise

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
