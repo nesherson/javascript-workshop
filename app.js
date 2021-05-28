e771736e01e78937a5df4144a9fef835a092d24f
unknown <nesad.omeragich27@gmail.com>
2021-05-28 03:24:29 +0200

    Handling different routes and parsing incoming requests

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const http = require('http');
-
 const express = require('express');
+const { urlencoded } = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+app.use(urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>'
+  );
 });
 
-const server = http.createServer(app);
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello World</h1>');
+});
+
+app.listen(3000);
