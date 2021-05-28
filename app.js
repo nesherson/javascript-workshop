77851a541f805042129248c2ebd22d2916c4d783
unknown <nesad.omeragich27@gmail.com>
2021-05-28 19:20:31 +0200

    Added and implemented express router

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/shop');
+
 const { urlencoded } = require('body-parser');
 
 const app = express();
 
 app.use(urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>'
-  );
-});
-
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello World</h1>');
-});
+app.use(adminRoutes);
+app.use(userRoutes);
 
 app.listen(3000);
