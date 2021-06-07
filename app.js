8bdaa375a54c12bd961a8241fe3cc1f0361e7224
unknown <nesad.omeragich27@gmail.com>
2021-06-07 23:53:55 +0200

    Adds initial views for admin and shop

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const path = require('path');
+const { urlencoded } = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.write('<h1>Hello World</h1>');
-  res.end();
-});
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const shopRoute = require('./routes/shop');
+const adminRoute = require('./routes/admin');
+
+app.use(shopRoute);
+app.use('/admin', adminRoute);
 
 app.listen(3000);
