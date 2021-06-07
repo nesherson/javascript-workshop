158d30e029f36510e8b017b3c6ced10d1a252675
unknown <nesad.omeragich27@gmail.com>
2021-06-07 23:53:15 +0200

    Adds product model

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathName = path.join(__dirname, 'data');
+
+const getFileContentFromPath = (cb) => {
+  fs.readFile(pathName, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, price, description) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+  }
+
+  save() {
+    getFileContentFromPath((products) => {
+      const updatedProducts = [...products, this];
+      fs.writeFile(pathName, JSON.stringify(updatedProducts), (err) => {
+        console.log('Product/save/err --> ', err);
+      });
+    });
+  }
+};
