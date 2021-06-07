555bd94aae7cb4ca5a324687845437d69dab19a5
unknown <nesad.omeragich27@gmail.com>
2021-06-07 22:37:57 +0200

    Adds initial project structure for Node-Express assignment

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-const path = require('path');
-
 const express = require('express');
-const { urlencoded } = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-const errorController = require('./controllers/error');
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-app.use(urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
-app.use(errorController.get404);
+app.get('/', (req, res, next) => {
+  res.write('<h1>Hello World</h1>');
+  res.end();
+});
 
 app.listen(3000);
