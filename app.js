a9d6308f781615d3ed2108f5c2040e26272d6208
unknown <nesad.omeragich27@gmail.com>
2021-06-08 02:20:17 +0200

    Adds page not found

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ app.set('views', 'views');
 const shopRoute = require('./routes/shop');
 const adminRoute = require('./routes/admin');
 
+const errorController = require('./controllers/error');
+
 app.use(shopRoute);
 app.use('/admin', adminRoute);
+app.use(errorController.get404);
 
 app.listen(3000);
