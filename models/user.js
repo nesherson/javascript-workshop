5aa99735a32d233758299f606f5aca0078c415cc
unknown <nesad.omeragich27@gmail.com>
2021-06-11 23:22:50 +0200

    Adds user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+// id, name, email
+
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/db');
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
