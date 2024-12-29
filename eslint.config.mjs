4dd49b1782a3dccf30134b3b4a78fe2302e6a57a
Nešad Omeragić <nesad.omeragich27@gmail.com>
2024-12-29 13:37:52 +0100

    Add functionality from tutorial

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": ["warning", {
+        "vars": "all",
+        "args": "after-used",
+        "caughtErrors": "all",
+        "ignoreRestSiblings": false,
+        "reportUsedIgnorePattern": false
+    }]
+    }
+  },
+  pluginJs.configs.recommended,
+];
